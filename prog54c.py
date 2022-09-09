dfa5677bd2f95448db067ef96efddac5de317687
ethanbol <112713153+ethanbol@users.noreply.github.com>
2022-09-09 21:15:01 +0000

    update

---
+++ prog54c.py
@@ -0,0 +1,11 @@
+radius=float(input("Please enter a number:" ))
+
+print("The radius is",radius)
+
+circumference= 2 * radius * 3.14159
+
+print("The Circumference is",circumference)
+
+area= radius * radius * 3.14159
+
+print("The area is", area)
