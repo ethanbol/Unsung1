94d14f08612de4feae16bbe0e5140fb5a810ec00
ethanbol <112713153+ethanbol@users.noreply.github.com>
2022-09-09 17:44:36 +0000

    update

---
+++ prog54b.py
@@ -0,0 +1,11 @@
+num1=float(input("please input number:" ))
+num2=float(input("please input number:" ))
+num3=float(input("please input number:" ))
+num4=float(input("please input number:" ))
+
+sum= num1 + num2 + num3 + num4
+
+average= num1 /4 + num2 /4 + num3 /4 + num4 /4
+
+print("the sum is",sum)
+print("the average is", average)
