a53935b340f4c2cc8379ba99f1b79e15785a19f8
ethanbol <112713153+ethanbol@users.noreply.github.com>
2022-09-09 14:36:54 +0000

    update

--- prog52a.py
+++ prog52a.py
@@ -4,8 +4,8 @@ width= int(input("Please enter the width: "))
 print("The length is",length)
 print("The width is", width)
 
-area = length + width
-perim = 2 + length + 2 + width 
+area = length * width
+perim = 2 * length + 2 * width 
 
 print ("The area is", area)
 print ("The perimeter is" ,perim)
