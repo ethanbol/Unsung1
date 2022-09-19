d5e124f71b792070d4000fd06d837e1af8e464ea
ethanbol <112713153+ethanbol@users.noreply.github.com>
2022-09-19 14:15:08 +0000

    update

---
+++ karel316.py
@@ -0,0 +1,88 @@
+from stanfordkarel import *
+
+
+class ktools:
+  def m(self):
+    """move"""
+    move()
+  
+  def tl(self):
+    """turn left"""
+    turn_left()
+
+  def tr(self):
+    """turn right"""
+    self.tl()
+    self.tl()
+    self.tl()
+
+  def ta(self):
+    """turn around"""
+    self.tl()
+    self.tl()
+
+  def pick(self):
+  
+    pick_beeper()
+
+
+  def put(self):
+    """put beeper"""
+    put_beeper()
+
+  def put2(self):
+    self.put()
+    self.m()
+    self.put()
+
+  def put5(self):
+    self.put2()
+    self.m()
+    self.put2()
+    self.m()
+    self.put()
+
+  
+    
+def main():
+    """ Karel code goes here! """
+    kt=ktools()
+    kt.tl()
+    kt.m()
+    kt.m()
+    kt.tr()
+    kt.m()
+    kt.m()
+    kt.pick()
+    kt.m()
+    kt.pick()
+    kt.m()
+    kt.pick()
+    kt.m()
+    kt.pick()
+    kt.tl()
+    kt.m()
+    kt.pick()
+    kt.m()
+    kt.pick()
+    kt.m()
+    kt.pick()
+    kt.tl()
+    kt.m()
+    kt.pick()
+    kt.m()
+    kt.pick()
+    kt.m()
+    kt.pick()
+    kt.tl()
+    kt.m()
+    kt.pick()
+    kt.m()
+    kt.pick()
+    kt.m()
+    kt.tl()
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
