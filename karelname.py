18e8744a6b621a07ad3bd4c6d976000748f5f04c
ethanbol <112713153+ethanbol@users.noreply.github.com>
2022-09-20 21:07:36 +0000

    update

--- karelname.py
+++ karelname.py
@@ -2,166 +2,163 @@ from stanfordkarel import *
 
 
 class ktools:
-  def m(self):
-    """move"""
-    move()
-  
-  def tl(self):
-    """turn left"""
-    turn_left()
 
-  def tr(self):
-    """turn right"""
-    self.tl()
-    self.tl()
-    self.tl()
+    def m(self):
+        """move"""
+        move()
 
-  def ta(self):
-    """turn around"""
-    self.tl()
-    self.tl()
+    def tl(self):
+        """turn left"""
+        turn_left()
 
-  def pick(self):
-  
-    pick_beeper()
+    def tr(self):
+        """turn right"""
+        self.tl()
+        self.tl()
+        self.tl()
+
+    def ta(self):
+        """turn around"""
+        self.tl()
+        self.tl()
+
+    def pick(self):
+
+        pick_beeper()
+
+    def put(self):
+        """put beeper"""
+        put_beeper()
+
+    def put2(self):
+        self.put()
+        self.m()
+        self.put()
+
+    def put5(self):
+        self.put2()
+        self.m()
+        self.put2()
+        self.m()
+        self.put()
+
+    def e(self):
+
+        self.m()
+        self.tl()
+        self.put5()
+        self.tr()
+        self.m()
+        self.put2()
+        self.tr()
+        self.m()
+        self.m()
+        self.tr()
+        self.put2()
+        self.tl()
+        self.m()
+        self.m()
+        self.tl()
+        self.put2()
+        self.m()
+
+    def h(self):
+        self.tl()
+        self.put5()
+        self.tr()
+        self.m()
+        self.m()
+        self.m()
+        self.tr()
+        self.put5()
+        self.ta()
+        self.m()
+        self.m()
+        self.tl()
+        self.m()
+        self.put2()
+        self.tl()
+        self.m()
+        self.m()
+        self.tl()
+        self.m()
+        self.m()
+        self.m()
+
+    def t(self):
+        self.m()
+        self.tl()
+        self.put5()
+        self.tl()
+        self.m()
+        self.tl()
+        self.m()
+        self.put()
+        self.tl()
+        self.m()
+        self.m()
+        self.put()
+        self.tr()
+        self.m()
+        self.m()
+        self.m()
+        self.tl()
+
+    def a(self):
+        self.tl()
+        self.put5()
+        self.tr()
+        self.m()
+        self.put()
+        self.m()
+        self.tr()
+        self.put5()
+        self.tr()
+        self.m()
+        self.tr()
+        self.m()
+        self.m()
+        self.put()
+        self.ta()
+        self.m()
+        self.m()
+        self.tl()
+        self.m()
+        self.m()
+
+    def n(self):
+
+        self.m()
+        self.tl()
+        self.put5()
+        self.tr()
+        self.m()
+        self.put()
+        self.m()
+        self.tr()
+        self.put5()
+        self.tl()
+        self.m()
 
 
-  def put(self):
-    """put beeper"""
-    put_beeper()
-
-  def put2(self):
-    self.put()
-    self.m()
-    self.put()
-
-  def put5(self):
-    self.put2()
-    self.m()
-    self.put2()
-    self.m()
-    self.put()
-
-  def e(self):
-
-    self.m()
-    self.tl()
-    self.put5()
-    self.tr()
-    self.m()
-    self.put2()
-    self.tr()
-    self.m()
-    self.m()
-    self.tr()
-    self.put2()
-    self.tl()
-    self.m()
-    self.m()
-    self.tl()
-    self.put2()
-    self.m()
-
-  def h(self):
-    self.tl()
-    self.put5()
-    self.tr()
-    self.m()
-    self.m()
-    self.m()
-    self.tr()
-    self.put5()
-    self.ta()
-    self.m()
-    self.m()
-    self.tl()
-    self.m()
-    self.put2()
-    self.tl()
-    self.m()
-    self.m()
-    self.tl()
-    self.m()
-    self.m()
-    self.m()
-    self.tl()
-    self.m()
-
-  def t(self):
-    self.m()
-    self.tl()
-    self.put5()
-    self.tl()
-    self.m()
-    self.tl()
-    self.m()
-    self.put()
-    self.tl()
-    self.m()
-    self.m()
-    self.put()
-    self.tr()
-    self.m()
-    self.m()
-    self.m()
-    self.tl()
-
-  def a(self):
-    self.tl()
-    self.put5()
-    self.tr()
-    self.m()
-    self.put()
-    self.m()
-    self.tr()
-    self.put5()
-    self.tr()
-    self.m()
-    self.tr()
-    self.m()
-    self.m()
-    self.put()
-    self.ta()
-    self.m()
-    self.m()
-    self.tl()
-    self.m()
-    self.m()
-
-  def n(self):
-
-    self.m()
-    self.tl()
-    self.put5()
-    self.tr()
-    self.m()
-    self.put()
-    self.m()
-    self.tl()
-    self.put5()
-    self.tl()
-    self.m()
-  
 def main():
     """ Karel code goes here! """
-    kt=ktools()
+    kt = ktools()
     kt.m()
     kt.tl()
     kt.m()
     kt.m()
     kt.tr()
     kt.e()
+    kt.m()
     kt.t()
     kt.m()
     kt.m()
-    kt.ta()
-    kt.m()
-    kt.m()
     kt.h()
+    kt.m()
     kt.a()
     kt.n()
-    
-    
+
     pass
 
 
