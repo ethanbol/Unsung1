9509a8960a41cb11724ce8a1abf13fe344355b6a
ethanbol <112713153+ethanbol@users.noreply.github.com>
2022-09-23 14:22:21 +0000

    update

---
+++ karel412.py
@@ -0,0 +1,134 @@
+from stanfordkarel import *
+
+
+class ktools:
+  def m(self):
+    """move"""
+    move()
+  
+  def tl(self):
+    """turn left"""
+    turn_left()
+
+  def tr(self):
+    """turn right"""
+    self.tl()
+    self.tl()
+    self.tl()
+
+  def ta(self):
+    """turn around"""
+    self.tl()
+    self.tl()
+
+  def pick(self):
+  
+    pick_beeper()
+
+
+  def put(self):
+    """put beeper"""
+    put_beeper()
+
+  def put2(self):
+    self.put()
+    self.m()
+    self.put()
+
+  def put5(self):
+    self.put2()
+    self.m()
+    self.put2()
+    self.m()
+    self.put()
+
+  def fic(self) -> bool:
+    """front is clear"""
+    return front_is_clear()
+
+
+  def fib(self) -> bool:
+    """front_is_blocked"""
+    return not self.fic()
+
+  def ric(self) -> bool:
+
+    self.tr()
+    if self.fic():
+      self.tl()
+      return True
+    self.tl()
+    return False 
+
+  def rib(self) -> bool:
+    """right is blocked"""
+    return not self.ric()
+    
+  def mazemove(self):
+    """maze move"""
+    if self.fib():
+      self.tl()
+    else:
+      self.m()
+      if self.ric():
+        self.tr()
+        self.m()
+        if self.ric():
+          self.tr()
+          self.m()
+    
+    pass
+
+  def mm(self, num):
+    for number in range(num):
+      self.m()
+
+
+  def pickm(self, num):
+    for i in range(num-1):
+      self.pick()
+      self.m()
+    self.pick()
+
+
+  def putm(self, num):
+    for _ in range(num-1):
+      self.put()
+      self.m()
+    self.put()
+
+  
+  
+  
+
+
+def main():
+    """ Karel code goes here! """
+    kt=ktools()
+    kt.m()
+    kt.tl()
+    kt.m()
+    kt.put()
+    kt.ta()
+    kt.m()
+    kt.tl()
+    kt.m()
+    kt.tl()
+    kt.m()
+    kt.putm(2)
+    kt.ta()
+    kt.mm(2)
+    kt.tl()
+    kt.m()
+    kt.tl()
+    kt.m()
+    kt.putm(3)
+    kt.ta()
+    kt.mm(3)
+    kt.tl()
+    kt.mm(6)
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
