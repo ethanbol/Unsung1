bbd1216e870204d12c2c050e7fc08ed47173fe73
ethanbol <112713153+ethanbol@users.noreply.github.com>
2022-11-08 14:51:39 +0000

    up

---
+++ fruitloops.py
@@ -0,0 +1,13 @@
+color = ["red","green","blue"]
+print(color)
+print(color[2])
+
+fruits = ["apples","bannana","orange","kiwi","mango"]
+for item in fruits:
+  print(item)
+
+for index in range(len(fruits)):
+  print(fruits[index])
+
+lastfruit = fruits[len(fruits)- 1]
+print(lastfruit)
---
+++ landocs58t.py
@@ -0,0 +1,4 @@
+a = int(3)
+dollor = - b +sprt(b**2-4(a)(c))
+b = int(5)
+c = int(-7)
