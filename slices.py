4045cf28e07ad7d0d606f52d992fc72067bb843c
ethanbol <112713153+ethanbol@users.noreply.github.com>
2022-11-10 15:09:04 +0000

    update

---
+++ slices.py
@@ -0,0 +1,11 @@
+list1 = [1,2,3,4,5]
+list2 = list1[0:3]
+print(list2)
+list3 = list1[3:len(list1)]
+print(list3)
+
+strhello = "hello, world!"
+substr = strhello[0:5]
+print(substr)
+print(strhello[-1])
+print(strhello[-6:-1])
