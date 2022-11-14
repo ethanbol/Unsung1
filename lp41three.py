dc150d72b383095835f422403a557a408135ea11
ethanbol <112713153+ethanbol@users.noreply.github.com>
2022-11-14 15:38:22 +0000

    u

--- lp41three.py
+++ lp41three.py
@@ -1,4 +1,19 @@
 def main():
+  eggs = int(input("Please enter number of eggs:"))
+  dozens = eggs % 12
+  extra = eggs - dozens
+  dozen = extra / 12
+  price = 0.0
+  
+
+  if dozen > 0 and dozen < 4:
+    price  = 0.50
+  if dozen > 4 and dozen <6:
+    price = 0.45
+  
+    
+
+  
   
   pass
 
