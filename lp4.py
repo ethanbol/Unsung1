e1caab4b850e1f31c600d80c3088c2c5452e4b68
ethanbol <112713153+ethanbol@users.noreply.github.com>
2022-11-15 15:23:20 +0000

    update

--- lp4.py
+++ lp4.py
@@ -1,22 +1,29 @@
 def main():
   eggs = int(input("Please enter number of eggs:"))
-  dozens = eggs % 12
-  extra = eggs - dozens
-  dozen = extra / 12
+  dozen = eggs // 12
+  remainder = eggs % 12
   price = 0.0
+  price2 = 0.0
   
 
   if dozen > 0 and dozen < 4:
     price  = 0.50
+    price2 = 0.25
   elif dozen > 4 and dozen <6:
     price = 0.45
+    price2 = 0.23
   elif dozen >6 and dozen <11:
     price = 0.40
+    price2 = 0.20
   elif dozen >11:
     price = 0.35
+    price = 0.18
+    
 
   total = price * dozen
-  extraprice =
+  remainder = price2
+  cost = total + remainder
+  print("Your total is ", cost)
     
 
   
