4aad6875a4ba0c6cd6c85611f4636a5424bbc41a
ethanbol <112713153+ethanbol@users.noreply.github.com>
2022-11-28 15:01:47 +0000

    uuuuuuu

---
+++ quiz3note.py
@@ -0,0 +1,15 @@
+text = input("enter text:")
+for index in range(len(text)):
+  print(text[index], end=" ")
+print()
+for letter in text:
+  print(letter)
+        
+def main():
+  text = "cool beans"
+  print(text)
+
+main()
+print("After main "+ text)
+  
+        
