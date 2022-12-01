1b020982124b2df16b4302b057719ac6545b2e24
ethanbol <112713153+ethanbol@users.noreply.github.com>
2022-12-01 15:37:15 +0000

    up

--- conways_game_of_life.py
+++ conways_game_of_life.py
@@ -1,7 +1,7 @@
 import copy, random, sys , time
 
 Width = 79 #grid width
-length = 20 #grid length
+Height = 20 #grid length
 
 Alive = '0' #living cell
 Dead = ' ' # dead cell
@@ -9,10 +9,31 @@ Dead = ' ' # dead cell
 nextcells = {}
 
 for x in range(Width):
-  for y in range(length):
+  for y in range(Height):
     if random.randint(0,1) == 0:
       nextcells[(x,y)] = Alive
     else:
       nextcells[(x,y)] = Dead
 while True:
-  
+  print('\n' * 50)
+  cells = copy.deepcopy(nextcells)
+
+for y in range(Height):
+  for x in range(Width):
+    print(cells[(x, y)], end='')
+  print()
+print("press Ctrl-C to quit.")
+
+for x in range(Width):
+  for y in range(Height):
+    left = (x - 1) % Width
+    right = (x + 1) % Width
+    above = (y - 1) % Height
+    below = (y + 1 ) %Height
+    numNeighbors = 0
+    if cells[(left ,above)] == Alive:
+      numNeighbors += 1
+    if cells[(x,above)] == Alive:\
+      numNeighbors += 1
+    if cells[(right, above)]
+
