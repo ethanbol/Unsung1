26cb0bd8156b41dec7e821fa9e82fdc075dd6bf7
ethanbol <112713153+ethanbol@users.noreply.github.com>
2022-12-07 15:08:24 +0000

    uppp

--- conways_game_of_life.py
+++ conways_game_of_life.py
@@ -1,5 +1,7 @@
 import copy, random, sys , time
 
+
+
 Width = 79 #grid width
 Height = 20 #grid length
 
@@ -33,7 +35,34 @@ for x in range(Width):
     numNeighbors = 0
     if cells[(left ,above)] == Alive:
       numNeighbors += 1
-    if cells[(x,above)] == Alive:\
+    if cells[(x,above)] == Alive:
       numNeighbors += 1
-    if cells[(right, above)]
+    if cells[(right, above)] == Alive:
+      numNeighbors += 1
+    if cells[(left, y)] == Alive:
+      numNeighbors += 1
+    if cells[(right, y)] == Alive:
+      numNeighbors += 1
+    if cells[(left, below)] == Alive:
+      numNeighbors += 1
+    if cells[(x, below)] == Alive:
+      numNeighbors += 1
+    if cells[(right, below)] == Alive:
+      numNeighbors += 1
+
+
+    if cells[(x,y)] == Alive and (numNeighbors == 2 or numNeighbors == 3):
+      nextcells[(x,y)] = Alive
+
+    elif cells[(x,y)] == Dead and numNeighbors == 3:
+      nextcells [(x,y)] = Alive
+    else:
+      nextcells[(x,y)] = Dead
+try:
+  time.sleep(1)
+except KeyboardInterrupt:
+  print("Conyays Game of Life ")
+  print("by Al Swiegart")
+  sys.exit()
+  
 
